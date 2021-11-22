5757f13fe0032cf84ee41ca5d43f5b7957d12c85
Shy <fleetwoodshybreasia@gmail.com>
2021-11-21 19:53:53 -0500

    final improvisions

--- script.js
+++ script.js
@@ -1,22 +1,95 @@
 // Assignment code here
-var passwordNumber = ['0', '1', '2', '3', '4','5', '6', '7', '8', '9'];
-var passwordCharacter = ['~', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-','=', '{', '[', '}', ']', ';', ':', ",", '>', '.', '/', '?' ];
-var passwordUppercase;['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var passwordLowercase;['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var submit;
-var selection;
+var passwordNumber = ['0123456789'];
+var passwordCharacter = ['!@#$%^&*()_+-='];
+var passwordUppercase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+var passwordLowercase = ['abcdefghijklmnopqrstuvwxyz'];
+var passLength = '';
+var randomPass = '';
 
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  //debugger;
+  passLength = window.prompt('Password Length? (choose a number 8 - 128)');
+  var passVal = parseInt(passLength);
+  if (Number.isNaN(passLength)) {
+    alert('Password must be a number')
+    generatePassword();
+  }
+  if (passVal < 8 || passVal > 128) {
+    window.alert('Password must be between 8 and 128');
+    generatePassword();
+  }
+
+  var numConfirm = window.confirm('Does your password include numbers?(OK for yes Cancel for no)');
+  if (numConfirm == true){
+    alert('Your password will include numbers');
+  }else {
+    alert('Your password will not include numbers');
+  }
+  
+  var charConfirm = window.confirm('Does your password include special characters?(OK for yes Cancel for no)');
+
+  var upperConfirm = window.confirm('Does your password include Uppercase (OK for yes Cancel for no)');
+
+  var lowerConfirm = window.confirm('Does your password include Lowercase (OK for yes Cancel for no)');
+
+if (numConfirm == false && charConfirm == false && upperConfirm == false && lowerConfirm == false){
+  alert('Must select atleast one criteria')
+return null
+}
+
+   var passInput = {
+     numConfirm: numConfirm,
+     charConfirm: charConfirm,
+     upperConfirm: upperConfirm,
+     lowerConfirm: lowerConfirm,
+     passVal: passVal,
+   };
+   return combineCriteria(passInput);
+  }
+
+  function combineCriteria(choices){
+    if (choices.numConfirm === true){
+      randomPass += numConfirm;
+    }
+
+    if (choices.charConfirm === true){
+      randomPass += charConfirm
+    }
+
+    if (choices.upperConfirm === true){
+      randomPass += upperConfirm
+    }
+
+    if (choices.lowerConfirm === true){
+      randomPass += lowerConfirm
+    }
+
+
+
+    var randomPass = '';
+    for (var i = 0; i < choices.passVal; i++){
+      randomPass =
+        randomPass +
+
+    randomPass.numConfirm(Math.floor(Math.random()*
+    randomPass.length));
+    }
+    return randomPass
+  };
+generateBtn.addEventListener('click', generatePassword);
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+   debugger;
+   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
+   passwordText.value = password;
+    return password
+ }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
